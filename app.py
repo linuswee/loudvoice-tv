102dcbfa3b41db45252fb08f51c5b4132b66836d
linuswee <linuswee@gmail.com>
2025-08-23 22:26:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ st.set_page_config(
     layout="wide"
 )
 
+st.markdown(
+    """
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/loudvoice_logo.png?v=2">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/loudvoice_logo.png?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/loudvoice_logo.png?v=2">
+    """,
+    unsafe_allow_html=True
+)
+
 # Inject favicon + iOS Home Screen icon into <head>
 st.markdown(
     """
