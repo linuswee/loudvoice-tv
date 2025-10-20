5ca5aef402018c232e231c44719dd2f1e07423a9
linuswee <linuswee@gmail.com>
2025-10-20 12:38:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1527,8 +1527,6 @@ with t1:
 with t2:
     now = datetime.now(LOCAL_TZ).strftime('%B %d, %Y %I:%M %p')
     st.markdown(f"<div class='timestamp'>{now}</div>", unsafe_allow_html=True)
-if not analytics_ok:
-    st.info("Using mock for YT 7‑day & country (Analytics call failed or not configured).")
 
 # =======================
 # Main layout
